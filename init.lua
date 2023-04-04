8488d20a4c8ae28f6fb58ab76c842b38172c4907
Kevin Schaul <kevin.schaul@gmail.com>
2023-04-04 10:03:26 -0500

    Add prettier, disable tsserver, reformat

--- init.lua
+++ init.lua
@@ -5,28 +5,25 @@
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
 local config = {
-
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin", -- remote to use
-    channel = "stable", -- "stable" or "nightly"
-    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "main", -- branch name (NIGHTLY ONLY)
-    commit = nil, -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false, -- skip prompts about breaking changes
+    remote = "origin",     -- remote to use
+    channel = "stable",    -- "stable" or "nightly"
+    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "main",       -- branch name (NIGHTLY ONLY)
+    commit = nil,          -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false,  -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false, -- automatically quit the current session after a successful update
+    auto_quit = false,     -- automatically quit the current session after a successful update
     -- remotes = { -- easily add new remotes to track
     --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
     --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
     --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
     -- },
   },
-
   -- Set colorscheme to use
   colorscheme = "astrodark",
-
   -- Add highlight groups in any theme
   highlights = {
     -- init = { -- this table overrides highlights in all themes
@@ -36,24 +33,23 @@ local config = {
     --   Normal = { bg = "#000000" },
     -- },
   },
-
   -- set vim options here (vim.<first_key>.<second_key> = value)
   options = {
     opt = {
       -- set to true or false etc.
       relativenumber = false, -- sets vim.opt.relativenumber
-      number = true, -- sets vim.opt.number
-      spell = false, -- sets vim.opt.spell
-      signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-      wrap = true, -- sets vim.opt.wrap
+      number = true,          -- sets vim.opt.number
+      spell = false,          -- sets vim.opt.spell
+      signcolumn = "auto",    -- sets vim.opt.signcolumn to auto
+      wrap = true,            -- sets vim.opt.wrap
     },
     g = {
-      mapleader = ",", -- sets vim.g.mapleader
-      autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-      cmp_enabled = true, -- enable completion at start
-      autopairs_enabled = true, -- enable autopairs at start
-      diagnostics_mode = 2, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-      icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+      mapleader = ",",                 -- sets vim.g.mapleader
+      autoformat_enabled = false,      -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
+      cmp_enabled = true,              -- enable completion at start
+      autopairs_enabled = true,        -- enable autopairs at start
+      diagnostics_mode = 2,            -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+      icons_enabled = true,            -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
       ui_notifications_enabled = true, -- disable notifications when toggling UI elements
       python3_host_prog = "~/.pyenv/versions/nvim/bin/python",
     },
@@ -72,7 +68,6 @@ local config = {
   diagnostics = {
     underline = true,
   },
-
   -- Extend LSP configuration
   lsp = {
     -- enable servers that you already have installed without mason
@@ -82,7 +77,7 @@ local config = {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = false, -- enable or disable format on save globally
+        enabled = false,    -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -91,7 +86,7 @@ local config = {
         },
       },
       disabled = { -- disable formatting capabilities for the listed language servers
-        -- "lua_ls",
+        "tsserver",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
@@ -135,7 +130,6 @@ local config = {
       -- },
     },
   },
-
   -- Mapping data with "desc" stored directly by vim.keymap.set().
   --
   -- Please use this mappings table to set keyboard mapping since this is the
@@ -155,12 +149,10 @@ local config = {
       ["<leader>b"] = { name = "Buffers" },
       -- quick save
       -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
-      
+
       [";"] = { ":" },
-      
       ["<S-l>"] = { "<cmd>bnext<cr>", desc = "Next buffer" },
       ["<S-h>"] = { "<cmd>bprevious<cr>", desc = "Previous buffer" },
-
       ["<leader>e"] = false,
       ["<leader>o"] = false,
       ["<leader>n"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" }
@@ -173,7 +165,6 @@ local config = {
       -- ["<esc>"] = false,
     },
   },
-
   -- Configure require("lazy").setup() options
   lazy = {
     defaults = { lazy = true },
@@ -191,7 +182,6 @@ local config = {
       },
     },
   },
-
   -- Configure plugins
   plugins = {
     -- You can disable default plugins as follows:
@@ -310,7 +300,7 @@ local config = {
     {
       "nvim-treesitter/nvim-treesitter",
       opts = {
-        -- ensure_installed = { "lua" },
+        ensure_installed = { "lua", "javascript" },
       },
     },
     -- use mason-lspconfig to configure LSP installations
@@ -326,7 +316,7 @@ local config = {
       "jay-babu/mason-null-ls.nvim",
       -- overrides `require("mason-null-ls").setup(...)`
       opts = {
-        -- ensure_installed = { "prettier", "stylua" },
+        ensure_installed = { "prettier" },
       },
     },
     {
@@ -344,7 +334,6 @@ local config = {
     -- { import = "astrocommunity.colorscheme.catppuccin" },
     -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   },
-
   -- Customize Heirline options
   heirline = {
     -- -- Customize different separators between sections
@@ -372,7 +361,6 @@ local config = {
     --   },
     -- },
   },
-
   -- This function is run last and is a good place to configuring
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
